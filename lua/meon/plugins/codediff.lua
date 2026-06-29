cfc4ee3253d52d5f0e6501f0ecebf010f386f780
kristian.jeremic <kristianjer@gmail.com>
2026-06-29 13:20:36 +0200

    update to use codediff

---
+++ lua/meon/plugins/codediff.lua
@@ -0,0 +1,7 @@
+return {
+	"esmuellert/codediff.nvim",
+	cmd = "CodeDiff",
+	keys = {
+		{ "<leader>gv", "<cmd>CodeDiff<cr>", desc = "CodeDiff open" },
+	},
+}
--- lua/meon/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sindrets/diffview.nvim",
-	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
-	keys = {
-		{ "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diffview open" },
-		{ "<leader>gV", "<cmd>DiffviewClose<cr>", desc = "Diffview close" },
-	},
-	opts = {},
-}
