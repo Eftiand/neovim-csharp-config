9a1b6e94ed040d043b55dd5b2c5d02eb89dc4de6
kristian.jeremic <kristianjer@gmail.com>
2026-07-01 22:50:12 +0200

    update codedigg

--- lua/meon/plugins/codediff.lua
+++ lua/meon/plugins/codediff.lua
@@ -4,4 +4,14 @@ return {
 	keys = {
 		{ "<leader>gv", "<cmd>CodeDiff<cr>", desc = "CodeDiff open" },
 	},
+	opts = {
+		keymaps = {
+			view = {
+				next_hunk = "n",
+				prev_hunk = "p",
+				next_file = "N",
+				prev_file = "P",
+			},
+		},
+	},
 }
