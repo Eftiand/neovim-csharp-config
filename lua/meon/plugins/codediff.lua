ccba12a3220dce3bc0f0b48bc31a71aa34612528
kristian.jeremic <kristianjer@gmail.com>
2026-09-01 15:42:44 +0200

    codediff

--- lua/meon/plugins/codediff.lua
+++ lua/meon/plugins/codediff.lua
@@ -2,7 +2,8 @@ return {
 	"esmuellert/codediff.nvim",
 	cmd = "CodeDiff",
 	keys = {
-		{ "<leader>gv", "<cmd>CodeDiff<cr>", desc = "CodeDiff open" },
+		{ "<leader>gv", "<cmd>CodeDiff origin/main...<cr>", desc = "CodeDiff vs origin/main (PR diff)" },
+		{ "<leader>gV", "<cmd>CodeDiff<cr>", desc = "CodeDiff working tree" },
 	},
 	opts = {
 		keymaps = {
